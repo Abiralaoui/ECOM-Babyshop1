983f2c2cba2cf6ba267d28836be9c197f02530a5
m2gi-belaouda <abdelhalimbelaoud@gmail.com>
2023-12-13 22:01:07 +0100

    [Ecom-front] Linking pagination with filter.

--- src/main/webapp/app/entities/produit/list/produit.component.ts
+++ src/main/webapp/app/entities/produit/list/produit.component.ts
@@ -79,7 +79,7 @@ export class ProduitComponent implements OnInit {
 
   ngOnInit(): void {
     this.load();
-   
+
     this.fetchCategories();
 
     this.activatedRoute.queryParamMap.pipe(
@@ -122,24 +122,22 @@ export class ProduitComponent implements OnInit {
   load(): void {
     const pageable = {
       'page': this.currentPage,
-      'size': 20
+      'size': 25
     };
 
-    this.loadFromBackendWithRouteInformations(pageable).subscribe({
+    this.loadFromBackendWithRouteInformations(pageable, this.criteria).subscribe({
       next: (res: EntityArrayResponseType) => {
         this.onResponseSuccess(res);
 
         this.cachedProducts = [];
         this.loadImagesForProducts();
         if (this.produits !== undefined) {
-         
+
           for (let i = 0; i < this.produits.length; i++) {
             this.produits[i].outOfStock = this.produits[i].stock === 0;
             this.cachedProducts.push(this.produits[i]);
-        
+
           }
-          console.log("iciii")
-          console.log(this.produits)
         }
       },
     });
@@ -203,7 +201,7 @@ export class ProduitComponent implements OnInit {
       this.panierService.ajouterAuPanier(produit);
       const modalRef = this.modalService.open(AddProduitPopupComponent);
       modalRef.componentInstance.produit =produit;
-    event.stopPropagation();
+      event.stopPropagation();
   }
 
   onCategoryChange(event: any, category: ICategory): void {
@@ -216,24 +214,9 @@ export class ProduitComponent implements OnInit {
     }
 
     this.criteria['categoryId.in'] = this.selectedCategories;
+    this.currentPage = 0;
 
-    this.produitService.fetchProductsByCriteria(this.criteria).subscribe({
-      next: (res: EntityArrayResponseType) => {
-        this.onResponseSuccess(res);
-
-        this.cachedProducts = [];
-        if (this.produits !== undefined) {
-          for (let i = 0; i < this.produits.length; i++) {
-            this.cachedProducts.push(this.produits[i]);
-            this.produits[i].outOfStock = this.produits[i].stock === 0;
-          
-
-          }
-          
-        }
-
-      }
-    });
+    this.load();
   }
 
   onPageChange(pageNumber: any): void {
@@ -251,7 +234,6 @@ export class ProduitComponent implements OnInit {
     return pages;
   }
 
-  // pagination.component.ts
   getPagesWithEllipsis(): (number | 'ellipsis')[] {
     const pages: (number | 'ellipsis') [] = [];
     const maxPages = 10;
@@ -339,10 +321,10 @@ export class ProduitComponent implements OnInit {
     return description;
   }
 
-  protected loadFromBackendWithRouteInformations(pageable?: any): Observable<EntityArrayResponseType> {
+  protected loadFromBackendWithRouteInformations(pageable?: any, criteria?:any): Observable<EntityArrayResponseType> {
     return combineLatest([this.activatedRoute.queryParamMap, this.activatedRoute.data]).pipe(
       tap(([params, data]) => this.fillComponentAttributeFromRoute(params, data)),
-      switchMap(() => this.queryBackend(this.predicate, this.ascending, pageable))
+      switchMap(() => this.queryBackend(this.predicate, this.ascending, pageable, criteria))
     );
   }
 
@@ -368,15 +350,13 @@ export class ProduitComponent implements OnInit {
     return data ?? [];
   }
 
-  protected queryBackend(predicate?: string, ascending?: boolean, pageable?: any): Observable<EntityArrayResponseType> {
+  protected queryBackend(predicate?: string, ascending?: boolean, pageable?: any, criteria?:any): Observable<EntityArrayResponseType> {
     this.isLoading = true;
-    const criteria = (this.selectedCategories.length === 0) ? {} :
-                      {key: 'category.in', value:this.selectedCategories};
 
     const queryObject = {
       eagerload: true,
       sort: this.getSortQueryParam(predicate, ascending),
-      criteria,
+      ...criteria,
       ...pageable
     };
     return this.produitService.query(queryObject).pipe(tap(() => (this.isLoading = false)));
@@ -407,7 +387,7 @@ export class ProduitComponent implements OnInit {
     });
 
     event.stopPropagation();
-  
+
     // You can pass data or subscribe to events from the modal
     // Example: modalRef.componentInstance.someData = yourData;
     // Example: modalRef.result.then((result) => { /* Handle modal result */ });
